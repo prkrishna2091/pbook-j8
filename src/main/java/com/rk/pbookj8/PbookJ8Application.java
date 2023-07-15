7985ee7fc16871329cd2bced6cb541f5beba9710
prkrishna2091 <prkrishna2091@gmail.com>
2023-07-15 08:58:33 +0530

    Initial commit

---
+++ src/main/java/com/rk/pbookj8/PbookJ8Application.java
@@ -0,0 +1,13 @@
+package com.rk.pbookj8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbookJ8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbookJ8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rk/pbookj8/PbookJ8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.pbookj8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbookJ8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
